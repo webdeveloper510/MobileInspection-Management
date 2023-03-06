0b80931e0515be49292f44c3432da6be58d5d35b
webdeveloper510 <developerweb6@gmail.com>
2023-03-06 16:46:38 +0530

    Update settings.py

--- Mobile_Inspection/settings.py
+++ Mobile_Inspection/settings.py
@@ -222,7 +222,7 @@ CLIENT_SECRET = "CLIENT_SECRET"
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 #465#
-EMAIL_HOST_USER = "deepika@codenomad.net"
-EMAIL_HOST_PASSWORD = "199428october"
+EMAIL_HOST_USER = "email host user"
+EMAIL_HOST_PASSWORD = "password"
 EMAIL_USE_TLS = True
 PASSWORD_RESET_TIMEOUT= 900 #sec=15 minutes, 
